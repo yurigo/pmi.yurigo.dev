345a2747619493e6cb2053b5a8dd2185f5799613
Yuri <ygonzoo@gmail.com>
2026-04-08 21:10:16 +0200

    fix: enhance dual light/dark mode styling for code blocks in Docs and Markdown layouts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig, passthroughImageService } from "astro/config";
-import tailwind from '@astrojs/tailwind';
-import remarkGithubAlerts from 'remark-github-alerts';
+import tailwind from "@astrojs/tailwind";
+import remarkGithubAlerts from "remark-github-alerts";
 
 export default defineConfig({
   integrations: [tailwind()],
   markdown: {
     remarkPlugins: [remarkGithubAlerts],
+    shikiConfig: {
+      themes: {
+        light: "one-light",
+        dark: "one-dark-pro",
+      },
+    },
   },
   image: {
     service: passthroughImageService(),
